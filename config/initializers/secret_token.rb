56904f2b3496235027879cc7999b486d87a703a9
kmazanec <k.mazanec@gmail.com>
2013-12-30 20:22:33 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrackMyLife::Application.config.secret_key_base = '8ed788b0415c2f0d93a84700c84c7c3735a845ce70a77072f5d4dbccafe50109d3217f437b5be3fc12eda32423103971159cdaa9650eb3c220b1e15384a1e589'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrackMyLife::Application.config.session_store :cookie_store, key: '_track_my_life_session'
